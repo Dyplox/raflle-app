0e4d25c351dc7c9b5a75e8c95d7435ae0d18842f
abelgutierrezarias <abel.gutierrez@ionix.cl>
2026-02-03 17:29:42 -0500

    chore: update base path to match new repo name Rifas-Pacorenos

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/raflle-app/', // Base path for GitHub Pages (your repo name)
+  base: '/Rifas-Pacorenos/', // Base path for GitHub Pages (your repo name)
 })
